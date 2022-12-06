861e85f42306cf2ecf8e2730304fd777a00fd396
Rizal Abdul Basith <rizalgu4rd14n@gmail.com>
2022-12-06 15:10:49 +0700

    activity dashboard dan splashscreen

--- app/src/main/java/com/mancal/mancal/MainActivity.kt
+++ app/src/main/java/com/mancal/mancal/MainActivity.kt
@@ -1,11 +1,23 @@
 package com.mancal.mancal
 
-import androidx.appcompat.app.AppCompatActivity
+import android.R.id.home
+import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
+import androidx.appcompat.app.AppCompatActivity
+
 
 class MainActivity : AppCompatActivity() {
+    private val waktu_loading = 4000
+
+    //4000=4 detik
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Handler().postDelayed({ //setelah loading maka akan langsung berpindah ke home activity
+            val home = Intent(this@MainActivity, Activity_Dashboard::class.java)
+            startActivity(home)
+            finish()
+        }, waktu_loading.toLong())
     }
 }
---
+++ app/src/main/java/com/mancal/mancal/ui/login/LoginResult.kt
@@ -0,0 +1,9 @@
+package com.mancal.mancal.ui.login
+
+/**
+ * Authentication result : success (user details) or error message.
+ */
+data class LoginResult (
+     val success:LoggedInUserView? = null,
+     val error:Int? = null
+)
