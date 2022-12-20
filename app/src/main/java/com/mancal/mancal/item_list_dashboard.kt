d645a39d0fc45c69ab6a2b8d3b7edf579a48e884
Vanesa <vanesaghea2@gmail.com>
2022-12-20 15:49:18 +0700

    progess card view

--- app/src/main/java/com/mancal/mancal/item_list_dashboard.kt
+++ app/src/main/java/com/mancal/mancal/item_list_dashboard.kt
@@ -3,10 +3,9 @@ package com.mancal.mancal
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class Activity_Dashboard : AppCompatActivity() {
+class item_list_dashboard : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_dashboard)
+        setContentView(R.layout.activity_item_list_dashboard)
     }
-    data class DataItem(var image: String?, var title: String?, var date: String?, var link: String?)
 }
