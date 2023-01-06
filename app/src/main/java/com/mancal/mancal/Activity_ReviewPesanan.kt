66c6e0ca5de64a601a5b2382b4d5ab82b38cd2ca
Vanesa <vanesaghea2@gmail.com>
2023-01-06 11:01:17 +0700

    tampilan riview pesanan

---
+++ app/src/main/java/com/mancal/mancal/Activity_ReviewPesanan.kt
@@ -0,0 +1,11 @@
+package com.mancal.mancal
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Activity_ReviewPesanan : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_review_pesanan)
+    }
+}
